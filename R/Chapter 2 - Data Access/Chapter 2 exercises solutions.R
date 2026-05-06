4f28ed97bd5477e6293e33e58a35eeb35d879432
DanieLLL5 <20211588@novaims.unl.pt>
2026-05-06 13:02:29 +0100

    Changes to Chapter 2 R script

--- R/Chapter 2 - Data Access/Chapter 2 exercises solutions.R
+++ R/Chapter 2 - Data Access/Chapter 2 exercises solutions.R
@@ -1,2 +1,44 @@
 # Chapter 2 - Data access exercises solutions
 
+# Accessing your current working directory
+getwd()
+
+# Changing the working directory to the folder of chapter 2
+setwd('C:/Users/danie/Downloads/P4B/R module/Chapter 2 - Data Access/Exercises')
+
+# Checking if the change was made
+
+# 1.
+#a) 
+smsales <- read.csv('Data/supermarket_sales.csv', header = TRUE)
+
+#b)
+write.table(smsales,'supermarket_sales1.txt')
+write.csv(smsales,'supermarket_sales1.csv')
+
+# Uncomment the cell below if openxlsx is not installed yet
+#install.packages("openxlsx") 
+library(openxlsx)
+write.xlsx(smsales, 'supermarket_sales1.xlsx')
+
+#c)
+# Uncomment the cell below if openxlsx is not installed
+#install.packages('openxlsx')
+library(openxlsx)
+smsales1 <- read_xlsx('supermarket_sales1.xlsx') # output is a dataframe
+
+# Another way of doing it with package readxl
+#install.packages('readxl')
+library(readxl)
+smsales1 <- read_xlsx('supermarket_sales1.xlsx') # output in tibble
+
+#d)
+smsales2 <- smsales1[smsales1$Product_Line == 'Sports and travel' &
+                       smsales1$Total > 150 &
+                       smsales1$Total < 250,
+                     c('Date', 'Branch', 'Rating', 'Total')
+]
+head(smsales2)
+
+#e)
+write.xlsx(smsales2, 'supermarket_sales2.xlsx')
