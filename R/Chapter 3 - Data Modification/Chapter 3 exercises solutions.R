e8fb1eab3bc0923232f73b61a300c221a84dbe0a
DanieLLL5 <20211588@novaims.unl.pt>
2026-05-13 13:12:09 +0100

    Added solutions of class 1 Week 10

--- R/Chapter 3 - Data Modification/Chapter 3 exercises solutions.R
+++ R/Chapter 3 - Data Modification/Chapter 3 exercises solutions.R
@@ -1,2 +1,32 @@
 # Chapter 3 exercises solutions
 
+# Before starting let's install and read the necessary libraries
+# Uncomment the cell below if tidyverse is not yet installed in your computer
+#install.packages('tidyverse') 
+library(tidyverse)
+library(dplyr)
+
+# Setting up the working directory
+setwd('C:/Users/danie/Downloads/P4B/R module/Chapter 3 - Data Modification/Exercises')
+getwd()
+
+# 1. Data Modification
+# 1.1.
+adidas_us_sales <- readRDS('Data/adidas_us_sales.rds')
+
+#a) 
+filter(adidas_us_sales, Total_Sales > 650000)
+filter(adidas_us_sales, City == 'Miami' | City == 'Los Angeles')
+filter(adidas_us_sales, quarter(Invoice_Date) == 3)
+filter(adidas_us_sales, Units_Sold > 700 & Total_Sales > 400000)
+filter(adidas_us_sales, Price_per_Unit < 20 | Price_per_Unit > 70)
+
+#b)
+arrange(adidas_us_sales, Total_Sales)
+
+#c)
+arrange(adidas_us_sales, desc(Operating_Profit))
+arrange(adidas_us_sales, Operating_Profit)
+
+#d)
+select(adidas_us_sales, Retailer, Invoice_Date, Total_Sales, Operating_Margin)
