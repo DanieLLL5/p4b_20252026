ce99a19b7a80a12d2e617e9b6316a4e4e9597544
DanieLLL5 <20211588@novaims.unl.pt>
2026-05-15 15:17:53 +0100

    Added exercises from class 2

--- R/Chapter 3 - Data Modification/Chapter 3 exercises solutions.R
+++ R/Chapter 3 - Data Modification/Chapter 3 exercises solutions.R
@@ -31,4 +31,46 @@ arrange(adidas_us_sales, Operating_Profit)
 #d)
 select(adidas_us_sales, Retailer, Invoice_Date, Total_Sales, Operating_Margin)
 
+#e)
+adidas_us_sales <- mutate(adidas_us_sales, Invoice_Year = year(Invoice_Date),
+                          Invoice_Month = month(Invoice_Date),
+                          Total_Sales_K = Total_Sales / 1e3,
+                          Operating_Margin_Perc = Operating_Margin * 100)
 
+#f)
+adidas_us_sales %>%
+  filter(Invoice_Year == 2021) %>%
+  filter(Operating_Profit == max(Operating_Profit, na.rm = TRUE)) %>%
+  select(Retailer, Product)
+
+# OR 
+adidas_us_sales %>%
+  filter(Invoice_Year == 2021) %>%
+  arrange(desc(Operating_Profit)) %>%
+  select(Retailer, Product) %>%
+  print(n = 1)
+
+#g)
+adidas_us_sales %>%
+  group_by(Retailer) %>%
+  summarize(n = n()) 
+
+#h)
+adidas_us_sales %>%
+  group_by(Invoice_Month) %>%
+  summarise(Avg_Total_Sales_K = mean(Total_Sales_K)) %>%
+  filter(Avg_Total_Sales_K == max(Avg_Total_Sales_K))
+
+# OR 
+adidas_us_sales %>%
+  group_by(Invoice_Month) %>%
+  summarise(Avg_Total_Sales_K = mean(Total_Sales_K)) %>%
+  arrange(-Avg_Total_Sales_K)
+
+# OR 
+adidas_us_sales %>%
+  group_by(Invoice_Month) %>%
+  mutate(Avg_Total_Sales_K = mean(Total_Sales_K)) %>%
+  arrange(desc(Avg_Total_Sales_K)) %>%
+  select(Invoice_Month, Avg_Total_Sales_K) %>%
+  print(n = 1)
